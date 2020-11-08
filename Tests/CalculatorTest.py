5d74ce7d225ec5f088bd9d4dd388072806b80be1
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-08 10:27:11 +0530

    Fix: path fixed for imports

--- Tests/CalculatorTest.py
+++ Tests/CalculatorTest.py
@@ -1,6 +1,6 @@
 import unittest
-from Calculator import Calculator
-from CsvReader import CsvReader
+from Calculator .Calculator import  Calculator
+from CsvReader.CsvReader import CsvReader
 
 
 class MyTestCase(unittest.TestCase):
