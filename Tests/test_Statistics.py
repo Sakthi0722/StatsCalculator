4930ef12bc0b39592732891d36cd38ab632526d1
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-08 19:32:06 +0530

    Task: Adding Statistics file for Testing and fixing bugs

--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -11,33 +11,35 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.statistics, Statistics)
 
     def test_mean_method(self):
-        test_data = CsvReader("Tests/Data/Statistics.csv").data
+        test_data = CsvReader("Tests/Data/Mean.csv").data
+        value_data = [int(row['Value 1']) for row in test_data]
         for row in test_data:
-            self.assertEqual(self.statistics.stats_mean(row['Value']), int(row['Mean']))
+            self.assertEqual(self.statistics.stats_mean(value_data), int(row['Mean']))
             self.assertEqual(self.statistics.result, int(row['Mean']))
 
     def test_median_method(self):
-        test_data = CsvReader("Tests/Data/Statistics.csv").data
+        test_data = CsvReader("Tests/Data/Median.csv").data
+        value_data = [int(row['Value 1']) for row in test_data]
         for row in test_data:
-            self.assertEqual(self.statistics.stats_median(row['Value']), int(row['Median']))
+            self.assertEqual(self.statistics.stats_median(value_data), int(row['Median']))
             self.assertEqual(self.statistics.result, int(row['Median']))
 
     def test_mode_method(self):
-        test_data = CsvReader("Tests/Data/Statistics.csv").data
+        test_data = CsvReader("Tests/Data/Mode.csv").data
         for row in test_data:
-            self.assertEqual(self.statistics.stats_mode(row['Value']), int(row['Mode']))
+            self.assertEqual(self.statistics.stats_mode(row['Value 1']), int(row['Mode']))
             self.assertEqual(self.statistics.result, int(row['Mode']))
 
     def test_Variance_method(self):
-        test_data = CsvReader("Tests/Data/Statistics.csv").data
+        test_data = CsvReader("Tests/Data/Variance.csv").data
         for row in test_data:
-            self.assertEqual(self.statistics.stats_variance(row['Value']), int(row['Variance']))
+            self.assertEqual(self.statistics.stats_variance(row['Value 1']), int(row['Variance']))
             self.assertEqual(self.statistics.result, round(float(row['Variance']), 4))
 
     def test_standard_deviation(self):
-        test_data = CsvReader("Tests/Data/Statistics.csv").data
+        test_data = CsvReader("Tests/Data/SD.csv").data
         for row in test_data:
-            self.assertEqual(self.statistics.stats_standard_deviation(row['Value']), int(row['Standard_Deviation']))
+            self.assertEqual(self.statistics.stats_standard_deviation(row['Value 1']), int(row['Standard_Deviation']))
             self.assertEqual(self.statistics.result, round(float(row['Standard_Deviation']), 5))
 
 
